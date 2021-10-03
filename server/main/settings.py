0e63cf1343d176467be121d84a8a1b4345403830
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-03 12:05:30 -0700

    feat(settings): add user app to django settings, and add media url for image uploads

--- server/main/settings.py
+++ server/main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = BASE_DIR / 'uploads'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
