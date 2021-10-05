c460b8cd7f56302b674af05e868bc46c440a72ee
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-04 19:15:04 -0700

    feat(foodtruck-urls): create urls for the truck and product views

---
+++ server/foodtruck/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.TruckListAPIView.as_view()),
+    path('<slug:slug>', views.TruckDetailAPIView.as_view()),
+    path('products', views.ProductListAPIView.as_view()),
+    path('products/<slug:slug>', views.ProductDetailAPIView.as_view()),
+]
