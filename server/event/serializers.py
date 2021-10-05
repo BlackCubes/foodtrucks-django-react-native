8fdb785509d221d86d69c08354cddf3b610f4709
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-05 13:06:36 -0700

    feat(event-serializer): create serializer for event model

---
+++ server/event/serializers.py
@@ -0,0 +1,22 @@
+from django.db.models import fields
+from rest_framework import serializers
+
+from .models import Event
+from foodtruck.models import Truck
+
+
+class EventSerializer(serializers.Serializer):
+    """
+    Serializer on the Event model.
+
+    Lookup Field: uuid.
+
+    Fields: uuid, date, start_time, end_time, and truck.
+    """
+    truck = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Truck.objects.all(), many=True)
+
+    class Meta:
+        model = Event
+        lookup_field = 'uuid'
+        fields = ('uuid', 'date', 'start_time', 'end_time', 'truck',)
