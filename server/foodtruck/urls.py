7bbb83fba58c0c682b3c547b05ba6cc0297ca3a0
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-04 19:31:28 -0700

    fix(foodtruck-urls): fix the product list url path

--- server/foodtruck/urls.py
+++ server/foodtruck/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.TruckListAPIView.as_view()),
     path('<slug:slug>', views.TruckDetailAPIView.as_view()),
-    path('products', views.ProductListAPIView.as_view()),
+    path('products/', views.ProductListAPIView.as_view()),
     path('products/<slug:slug>', views.ProductDetailAPIView.as_view()),
 ]
