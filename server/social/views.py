7aa372e57193c9f727e48c9daf63c543521021bc
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-06 09:51:44 -0700

    feat(social-views): create views for the Like and Emoji models and serializers

--- server/social/views.py
+++ server/social/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Emoji, Like
+from .serializers import EmojiSerializer, LikeSerializer
+
+
+# Emoji views:
+class EmojiListAPIView(generics.ListAPIView):
+    """
+    API view to retrieve a list from the Emoji model.
+
+    Request Type: GET.
+    """
+    queryset = Emoji.objects.all()
+    serializer_class = EmojiSerializer
+
+
+class EmojiDetailAPIView(generics.RetrieveAPIView):
+    """
+    API view to retrieve one from the Emoji model based on its uuid.
+
+    Lookup Field: uuid.
+
+    Request Type: GET.
+    """
+    queryset = Emoji.objects.all()
+    lookup_field = 'uuid'
+    serializer_class = EmojiSerializer
+
+
+# Like views:
+class LikeCreateAPIView(generics.CreateAPIView):
+    """
+    API view to either create (or update from the custom method) from the Like model.
+
+    Request Type: POST.
+    """
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+
+class LikeDetailUpdateAPIView(generics.RetrieveUpdateAPIView):
+    """
+    API view to retrieve or update from the Like model based on its uuid.
+
+    Lookup Field: uuid.
+
+    Request Type: GET, PUT, and PATCH.
+    """
+    queryset = Like.objects.all()
+    lookup_field = 'uuid'
+    serializer_class = LikeSerializer
