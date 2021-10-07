4cecfecca3833fd7304eb33f4bde66d5ed0d9573
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-07 07:53:40 -0700

    feat(review-serializer): create serializer for the Review model

---
+++ server/review/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from .models import Review
+from foodtruck.models import Product
+from user.models import CustomUser
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """
+    Serializer on the Review model.
+
+    Lookup Field: uuid.
+
+    Fields: uuid, review, created_at, updated_at, product, and user.
+    """
+    product = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Product.objects.all())
+    user = serializers.SlugRelatedField(
+        slug_field='uuid', queryset=CustomUser.objects.all())
+
+    class Meta:
+        model = Review
+        lookup_field = 'uuid'
+        fields = ('uuid', 'review', 'created_at',
+                  'updated_at', 'product', 'user',)
