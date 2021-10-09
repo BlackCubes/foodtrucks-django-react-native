21df0b1d85c9e6595a64a949ffc9a20a733108bf
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-08 22:27:31 -0700

    fix(django-cors): add CORS for localhost:3000, and add allow hosts for 10.0.0.233

--- server/main/settings.py
+++ server/main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z*&k=x^^dyz#3*y5&x-6*_uogw*73s3w$cr_+08zzu44oog0q_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.0.233', ]
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Installed Apps
+    'corsheaders',
     'rest_framework',
     'phonenumber_field',
     # My Apps
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,6 +131,11 @@ USE_TZ = True
 # Phonenumber Field Format
 PHONENUMBER_DB_FORMAT = 'NATIONAL'
 PHONENUMBER_DEFAULT_REGION = 'US'
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 
 # Static files (CSS, JavaScript, Images)
