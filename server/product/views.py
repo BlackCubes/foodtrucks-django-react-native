34781a3027effc6c2c306290a971910270871e2a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-10 13:18:28 -0700

    feat(product-views): create views for product

--- server/product/views.py
+++ server/product/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductListAPIView(generics.ListAPIView):
+    """
+    API view to retrieve a list from the product model.
+
+    Request Type: GET.
+    """
+    queryset = Product.objects.all().order_by('slug')
+    serializer_class = ProductSerializer
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    """
+    API view to retrieve one from the Product model based on its slug.
+
+    Lookup Field: slug.
+
+    Request Type: GET.
+    """
+    queryset = Product.objects.all().order_by('slug')
+    lookup_field = 'slug'
+    serializer_class = ProductSerializer
