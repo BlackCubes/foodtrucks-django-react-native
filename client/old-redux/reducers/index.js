7808de92e71e699e25f7a88012138a58ddebe0fd
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-09 22:08:18 -0700

    feat(root-reducer): add social to the root reducer

--- client/old-redux/reducers/index.js
+++ client/old-redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
 import foodtruckReducer from './foodtruckReducer';
+import socialReducer from './socialReducer';
 
 const rootReducer = combineReducers({
   foodtruck: foodtruckReducer,
   // product: {},
+  social: socialReducer,
 });
 
 export default rootReducer;
