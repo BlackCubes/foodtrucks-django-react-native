626955752f3f05f4a71f8931893bf959adf0981c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-11 12:03:05 -0700

    feat(product-likes-url): create nested route to retrieve all likes from the product's slug

--- server/product/urls.py
+++ server/product/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.ProductListAPIView.as_view()),
     path('<slug:slug>', views.ProductDetailAPIView.as_view()),
+    re_path(r'^(?P<product_slug>[\w-]+)/socials/?$',
+            views.ProductLikesModelViewSet.as_view({'get': 'list'})),
 ]
