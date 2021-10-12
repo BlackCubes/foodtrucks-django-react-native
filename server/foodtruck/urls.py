7708d05df5913bd00ff40366615857c56a127ca5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-11 17:34:57 -0700

    feat(truck-likes-url): create nested route to retrieve all likes from the truck's slug

--- server/foodtruck/urls.py
+++ server/foodtruck/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.TruckListAPIView.as_view()),
     path('<slug:slug>', views.TruckDetailAPIView.as_view()),
+    re_path(r'^(?P<truck_slug>[\w-]+)/socials/?$',
+            views.TruckLikesModelViewSet.as_view({'get': 'list'})),
 ]
