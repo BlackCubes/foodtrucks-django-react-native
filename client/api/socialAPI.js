0b32deb9662bd0e8f386c226903c42a6b7790165
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-11 18:56:00 -0700

    refactor(social-api): change the axiosInit into a funtion

--- client/api/socialAPI.js
+++ client/api/socialAPI.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
-const axiosInit = axios.create({
-  baseURL: 'http://10.0.0.233:8000/api/v1/socials/',
-  responseType: 'json',
-});
+const axiosInit = (path) =>
+  axios.create({
+    baseURL: `http://10.0.0.233:8000/api/v1/${path}/`,
+    responseType: 'json',
+  });
 
 export const getAllSocials = (headers) =>
   new Promise((resolve, reject) =>
     setTimeout(() => {
       try {
-        axiosInit
+        axiosInit('socials')
           .get('', headers)
           .then((res) => resolve(res.data))
           .catch((err) => {
@@ -31,7 +32,7 @@ export const createSocial = (emoji, product_slug, like, headers) =>
   new Promise((resolve, reject) =>
     setTimeout(() => {
       try {
-        axiosInit
+        axiosInit('socials')
           .post(
             '',
             {
