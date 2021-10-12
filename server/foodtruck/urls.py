c67b64a2a8e098d10ebf9ae2669ae80bdf908280
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-12 10:03:53 -0700

    feat(truck-product-urls): create nested route for getting all products on foodtruck's slug

--- server/foodtruck/urls.py
+++ server/foodtruck/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.TruckListAPIView.as_view()),
     path('<slug:slug>', views.TruckDetailAPIView.as_view()),
+    re_path(r'^(?P<truck_slug>[\w-]+)/products/?$',
+            views.TruckProductsModelViewSet.as_view({'get': 'list'})),
     re_path(r'^(?P<truck_slug>[\w-]+)/socials/?$',
             views.TruckLikesModelViewSet.as_view({'get': 'list'})),
 ]
