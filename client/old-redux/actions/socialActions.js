a1f4a0350f89c2912b803aa9fcae982a815fc010
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-11 19:15:46 -0700

    feat(social-actions): create actions for foodtruck and product on social

--- client/old-redux/actions/socialActions.js
+++ client/old-redux/actions/socialActions.js
@@ -1,5 +1,15 @@
-import { ADD_SOCIAL, GET_ALL_SOCIALS } from '../constants/socialTypes';
-import { createSocial, getAllSocials } from '../../api/socialAPI';
+import {
+  ADD_SOCIAL,
+  GET_ALL_FOODTRUCK_SOCIALS,
+  GET_ALL_PRODUCT_SOCIALS,
+  GET_ALL_SOCIALS,
+} from '../constants/socialTypes';
+import {
+  createSocial,
+  getAllSocials,
+  getAllSocialsFromFoodtruck,
+  getAllSocialsFromProduct,
+} from '../../api/socialAPI';
 import { headers } from '../../utils';
 
 // GET ALL SOCIALS
@@ -24,4 +34,26 @@ export const addSocial = (emoji, product_slug, like) => (dispatch) =>
     .then((res) =>
       dispatch(finishAddSocial(res.uuid, res.like, res.emoji, res.product))
     )
+    .catch((err) => console.error(err));
+
+// GET ALL SOCIALS FROM FOODTRUCK
+export const finishAllSocialsFromFoodtruck = (foodtruckSocials) => ({
+  type: GET_ALL_FOODTRUCK_SOCIALS,
+  payload: { foodtruckSocials },
+});
+
+export const allSocialsFromFoodtruck = (foodtruck_slug) => (dispatch) =>
+  getAllSocialsFromFoodtruck(foodtruck_slug, headers())
+    .then((res) => dispatch(finishAllSocialsFromFoodtruck(res)))
+    .catch((err) => console.error(err));
+
+// GET ALL SOCIALS FROM PRODUCT
+export const finishAllSocialsFromProduct = (productSocials) => ({
+  type: GET_ALL_PRODUCT_SOCIALS,
+  payload: { productSocials },
+});
+
+export const allSocialsFromProduct = (product_slug) => (dispatch) =>
+  getAllSocialsFromProduct(product_slug, headers())
+    .then((res) => dispatch())
     .catch((err) => console.error(err));
