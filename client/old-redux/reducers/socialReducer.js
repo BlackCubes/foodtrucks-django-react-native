9d904636ae9e92816d026a5b304ed4da4872629c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-11 19:29:15 -0700

    refactor(social-reducers): remove and comment out any relation to state.socials

--- client/old-redux/reducers/socialReducer.js
+++ client/old-redux/reducers/socialReducer.js
@@ -6,7 +6,6 @@ import {
 } from '../constants/socialTypes';
 
 const INITIAL_STATE = {
-  socials: [],
   foodtruckSocials: [],
   productSocials: [],
 };
@@ -29,34 +28,33 @@ const socialReducer = (state = INITIAL_STATE, action) => {
         productSocials: action.payload.productSocials,
       };
     case ADD_SOCIAL:
-      let uuidExists = false;
+      // let uuidExists = false;
 
-      let newSocials = state.socials.map((social) => {
-        const clonedSocial = { ...social };
+      // let newSocials = state.socials.map((social) => {
+      //   const clonedSocial = { ...social };
 
-        if (clonedSocial.uuid === action.payload.uuid) {
-          clonedSocial.like += action.payload.like;
-          uuidExists = true;
-        }
+      //   if (clonedSocial.uuid === action.payload.uuid) {
+      //     clonedSocial.like += action.payload.like;
+      //     uuidExists = true;
+      //   }
 
-        return clonedSocial;
-      });
+      //   return clonedSocial;
+      // });
 
-      newSocials = uuidExists
-        ? newSocials
-        : [
-            ...state.socials,
-            {
-              uuid: action.payload.uuid,
-              like: action.payload.like,
-              emoji: action.payload.emoji,
-              product: action.payload.product,
-            },
-          ];
+      // newSocials = uuidExists
+      //   ? newSocials
+      //   : [
+      //       ...state.socials,
+      //       {
+      //         uuid: action.payload.uuid,
+      //         like: action.payload.like,
+      //         emoji: action.payload.emoji,
+      //         product: action.payload.product,
+      //       },
+      //     ];
 
       return {
         ...state,
-        socials: newSocials,
       };
     default:
       return state;
