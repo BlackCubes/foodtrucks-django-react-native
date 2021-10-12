51931dfba870ec4ec5961c750952de7e4a7ab0fe
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-12 13:57:55 -0700

    feat(review-api): create apis for the review

---
+++ client/api/reviewAPI.js
@@ -0,0 +1,119 @@
+import { axiosInit } from '../utils';
+
+export const getAllReviews = (headers) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      try {
+        axiosInit('reviews')
+          .get('', headers)
+          .then((res) => resolve(res.data))
+          .catch((err) => {
+            if (err.response) {
+              reject(err.response.data);
+            } else if (err.request) {
+              reject(err.request);
+            } else {
+              reject(err.message);
+            }
+          });
+      } catch (err) {
+        reject('System error. Please try again later.');
+      }
+    }, 1000)
+  );
+
+export const createReview = (review, product_slug, user_uuid, headers) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      try {
+        axiosInit('reviews')
+          .post(
+            '',
+            {
+              review,
+              product: product_slug,
+              user: user_uuid,
+            },
+            headers
+          )
+          .then((res) => resolve(res.data))
+          .catch((err) => {
+            if (err.response) {
+              reject(err.reponse.data);
+            } else if (err.request) {
+              reject(err.request);
+            } else {
+              reject(err.message);
+            }
+          });
+      } catch (err) {
+        reject('System error. Please try again later.');
+      }
+    }, 1000)
+  );
+
+export const getReview = (slug, headers) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      try {
+        axiosInit('reviews')
+          .get(`${slug}`, headers)
+          .then((res) => resolve(res.data))
+          .catch((err) => {
+            if (err.response) {
+              reject(err.reponse.data);
+            } else if (err.request) {
+              reject(err.request);
+            } else {
+              reject(err.message);
+            }
+          });
+      } catch (err) {
+        reject('System error. Please try again later.');
+      }
+    }, 1000)
+  );
+
+export const getAllReviewsFromFoodtruck = (foodtruck_slug, headers) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      try {
+        axiosInit('foodtrucks')
+          .get(`${foodtruck_slug}/reviews`, headers)
+          .then((res) => resolve(res.data))
+          .catch((err) => {
+            if (err.response) {
+              reject(err.reponse.data);
+            } else if (err.request) {
+              reject(err.request);
+            } else {
+              reject(err.message);
+            }
+          });
+      } catch (err) {
+        reject('System error. Please try again later.');
+      }
+    }, 1000)
+  );
+
+export const getAllReviewsFromProduct = (product_slug, headers) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      try {
+        axiosInit('products')
+          .get(`${product_slug}/reviews`, headers)
+          .then((res) => resolve(res.data))
+          .catch((err) => {
+            if (err.response) {
+              reject(err.response.data);
+            } else if (err.request) {
+              reject(err.request);
+            } else {
+              reject(err.message);
+            }
+          });
+      } catch (err) {
+        reject('System error. Please try again later.');
+      }
+    }, 1000)
+  );
