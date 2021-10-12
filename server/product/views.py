70c1cb25893cb18c099711e4249a07060afced4a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-12 12:34:30 -0700

    feat(product-review-views): create custom viewset on Review and Product models

--- server/product/views.py
+++ server/product/views.py
@@ -3,6 +3,8 @@ from rest_framework.exceptions import NotFound
 
 from .models import Product
 from .serializers import ProductSerializer
+from review.models import Review
+from review.serializers import ReviewSerializer
 from social.models import Like
 from social.serializers import LikeSerializer
 
@@ -42,6 +44,30 @@ class ProductLikesModelViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all().select_related(
         'product').select_related('emoji').order_by('product__slug')
     serializer_class = LikeSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        product_slug = self.kwargs.get('product_slug')
+
+        try:
+            product = Product.objects.get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise NotFound('A product with this slug does not exist.')
+
+        return self.queryset.filter(product=product)
+
+
+class ProductReviewsModelViewSet(viewsets.ModelViewSet):
+    """
+    Model viewset on the Review and Product models. Tries to retrieve all
+    reviews based on the product's slug, if a GET request.
+
+    Actions: list, create, retrieve, update, partial_update, destroy.
+
+    Request Like: GET, POST, PATCH, PUT, DELETE.
+    """
+    queryset = Review.objects.all().select_related(
+        'product').select_related('user').order_by('created_at')
+    serializer_class = ReviewSerializer
 
     def get_queryset(self, *args, **kwargs):
         product_slug = self.kwargs.get('product_slug')
