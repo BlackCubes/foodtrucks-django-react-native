999ec98368a94949d66da218d8262d6585ef8b8b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-11 19:05:12 -0700

    feat(social-types): create types for foodtruck and product on social

--- client/old-redux/constants/socialTypes.js
+++ client/old-redux/constants/socialTypes.js
@@ -1,2 +1,4 @@
 export const GET_ALL_SOCIALS = 'socials/get-all';
 export const ADD_SOCIAL = 'socials/add';
+export const GET_ALL_FOODTRUCK_SOCIALS = 'socials/foodtruck-get-all';
+export const GET_ALL_PRODUCT_SOCIALS = 'socials/product-get-all';
