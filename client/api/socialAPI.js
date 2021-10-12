efff69fc87ffa0e80abdf5fc1a079e3cac995edd
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-11 18:58:18 -0700

    refactor(foodtruck-social-apis): move the function from foodtruckAPI to socialAPI

--- client/api/socialAPI.js
+++ client/api/socialAPI.js
@@ -57,3 +57,25 @@ export const createSocial = (emoji, product_slug, like, headers) =>
       }
     }, 1000)
   );
+
+export const getAllSocialsFromFoodtruck = (slug, headers) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      try {
+        axiosInit('foodtrucks')
+          .get(`${slug}/socials`, headers)
+          .then((res) => resolve(res.data))
+          .catch((err) => {
+            if (err.response) {
+              reject(err.response.data);
+            } else if (err.request) {
+              reject(err.request);
+            } else {
+              reject(err.message);
+            }
+          });
+      } catch (err) {
+        reject('System error. Please try again later.');
+      }
+    }, 1000)
+  );
