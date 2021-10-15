6effe6949797abdae4f1c7b44e1abc2c0d4660c3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-14 20:55:03 -0700

    refactor(app): remove code

--- client/App.js
+++ client/App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -10,22 +8,9 @@ import AppRouter from './router';
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <NavigationContainer>
-          <AppRouter />
-        </NavigationContainer>
-
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <AppRouter />
+      </NavigationContainer>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
