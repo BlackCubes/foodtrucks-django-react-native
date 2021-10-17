52cc09e2f03319cd9aad8bbc826bee05e97778e5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-17 07:55:03 -0700

    feat(truck-model): add dunder string method to display the name of the truck

--- server/foodtruck/models.py
+++ server/foodtruck/models.py
@@ -42,6 +42,9 @@ class Truck(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class TruckImage(models.Model):
     """
