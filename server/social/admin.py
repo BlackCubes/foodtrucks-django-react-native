1c7a0b0724901f25d7424cd8e4be3396b90af571
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-17 18:13:19 -0700

    feat(emoji-admin): create admin for emoji

--- server/social/admin.py
+++ server/social/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Emoji, Like
+
+
+class EmojiAdmin(admin.ModelAdmin):
+    """"""
+    model = Emoji
+
+    # View all emojis
+    list_display = ('emoji', 'name', 'created_at', 'updated_at',)
+    list_display_links = ('emoji', 'name',)
+    list_filter = ('emoji', 'name',)
+    search_fields = ('emoji', 'name',)
+    ordering = ('name',)
+
+    # View and changing one emoji
+    fieldsets = (
+        (None, {'fields': ('emoji', 'name',)}),
+        ('Additional Info', {'fields': ('uuid', 'created_at', 'updated_at',)}),
+    )
+    readonly_fields = ('uuid', 'created_at', 'updated_at',)
+
+    # Adding one new emoji
+    add_fieldset = (
+        (None, {'fields': ('emoji', 'name')}),
+    )
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldset
+        return super(EmojiAdmin, self).get_fieldsets(request, obj)
+
+
+admin.site.register(Emoji, EmojiAdmin)
