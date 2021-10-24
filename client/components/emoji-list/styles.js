e8796b2bcb7f3d2c2c82395f9d2d3e7359e996af
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-23 21:53:05 -0700

    feat(emoji-list-styles): create styles for emoji list

---
+++ client/components/emoji-list/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 5,
+  },
+  emoji_wrapper: {
+    marginRight: 5,
+    padding: 2,
+  },
+  emoji: {
+    fontSize: 21,
+  },
+});
+
+export default styles;
