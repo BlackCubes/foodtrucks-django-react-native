913e1c5cb3fd08fd31c2c5cf8c9c26739678c79d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-23 21:54:18 -0700

    feat(emoji-list): add custom styles, and add animations

--- client/components/emoji-list/index.js
+++ client/components/emoji-list/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { Animated, Pressable, Text, View } from 'react-native';
+
+import styles from './styles';
 
 const EmojiList = ({ addSocial, emojis, productSlug }) => {
+  const onPressedIn = (scaling, translation) => {
+    Animated.timing(scaling, {
+      toValue: 1.2,
+      duration: 50,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(translation, {
+      toValue: -7,
+      duration: 50,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const onPressedOut = (scaling, translation) => {
+    Animated.timing(scaling, {
+      toValue: 1,
+      duration: 50,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(translation, {
+      toValue: 0,
+      duration: 50,
+      useNativeDriver: true,
+    }).start();
+  };
+
   if (!emojis.length) return <Text>No emojis!</Text>;
 
-  return emojis.map((emoji) => (
-    <View key={emoji.uuid}>
-      <Button
-        title={emoji.emoji}
-        onPress={() => addSocial(emoji.emoji, productSlug, 1)}
-      />
+  return (
+    <View style={styles.container}>
+      {emojis.map((emoji) => {
+        const scaling = useRef(new Animated.Value(1)).current;
+        const translation = useRef(new Animated.Value(0)).current;
+
+        return (
+          <View key={emoji.uuid} style={styles.emoji_wrapper}>
+            <Pressable
+              onPressIn={() => onPressedIn(scaling, translation)}
+              onPressOut={() => onPressedOut(scaling, translation)}
+              onPress={() => addSocial(emoji.emoji, productSlug, 1)}
+            >
+              <Animated.Text
+                style={{
+                  ...styles.emoji,
+                  transform: [{ translateY: translation }, { scale: scaling }],
+                }}
+              >
+                {emoji.emoji}
+              </Animated.Text>
+            </Pressable>
+          </View>
+        );
+      })}
     </View>
-  ));
+  );
 };
 
 export default EmojiList;
