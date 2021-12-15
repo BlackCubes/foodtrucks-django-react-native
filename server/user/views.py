41b357a296fef5f76fb79b615113a31852576c46
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-12-15 10:48:06 -0800

    feat(user-login-view): create API view for user login, and add more docs to the RegisterAPIView

--- server/user/views.py
+++ server/user/views.py
@@ -3,12 +3,41 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegisterSerializer
+from .serializers import LoginSerializer, RegisterSerializer
+
+
+class LoginAPIView(APIView):
+    """
+    API view to login the user if it exists in the User model.
+
+    Permissions: Allow any.
+
+    Serializer: LoginSerializer.
+
+    Request Type: POST.
+    """
+    permission_classes = (AllowAny,)
+    serialzer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serialzer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        data = {
+            'status': 'success',
+            'data': serializer.data,
+        }
+
+        return Response(data=data, status=status.HTTP_200_OK)
 
 
 class RegisterAPIView(APIView):
     """
     API view to create a new user to the User model.
+
+    Permissions: Allow any.
+
+    Serializer: RegisterSerializer.
 
     Request Type: POST.
     """
