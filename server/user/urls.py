3af6e73c79986dd75da8af5921c0558b7fd6badd
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-12-15 12:58:53 -0800

    feat(user-urls): create urls for the user profile

--- server/user/urls.py
+++ server/user/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('register/', views.RegisterAPIView.as_view()),
     path('login/', views.LoginAPIView.as_view()),
+    path('profile/', views.UserProfileRetrieveUpdateAPIView.as_view()),
 ]
