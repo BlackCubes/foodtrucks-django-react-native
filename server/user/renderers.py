af4ab160d3a02835dd936f9fb716b771e30c73d4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-12-15 16:20:46 -0800

    feat(user-renderers): decode the incoming token for the JSON rendering

--- server/user/renderers.py
+++ server/user/renderers.py
@@ -12,6 +12,11 @@ class UserJSONRenderer(JSONRenderer):
     charset = 'utf-8'
 
     def render(self, data, media_type=None, renderer_context=None):
+        token = data.get('token', None)
+
+        if token is not None and isinstance(token, bytes):
+            data['token'] = token.decode('utf-8')
+
         return json.dumps({
             'data': data,
         })
