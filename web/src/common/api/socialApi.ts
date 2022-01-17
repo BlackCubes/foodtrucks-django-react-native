afe6e6cc9c16450e438618313640042aa17c3036
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-01-16 20:06:04 -0800

    feat(social-api-rtk): create RTK Query functionality on fetching the 'social' API endpoint from Django server

---
+++ web/src/common/api/socialApi.ts
@@ -0,0 +1,51 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+
+import customBaseQuery from './customBaseQuery';
+import { Emoji, Social } from '../models';
+
+export const socialApi = createApi({
+  reducerPath: 'socialApi',
+  baseQuery: customBaseQuery,
+
+  keepUnusedDataFor: 0,
+  refetchOnMountOrArgChange: true,
+  refetchOnReconnect: true,
+
+  tagTypes: ['Social', 'Emoji'],
+
+  endpoints: (builder) => ({
+    getSocials: builder.query<Social[], void>({
+      query: () => ({ url: '/socials' }),
+      providesTags: ['Social'],
+    }),
+
+    getSocialByUuid: builder.query<Social, string>({
+      query: (uuid) => ({ url: `/socials/${uuid}` }),
+      providesTags: ['Social'],
+    }),
+
+    createSocial: builder.mutation<
+      Social,
+      Pick<Social, 'emoji' | 'like' | 'product'>
+    >({
+      query: (payload) => ({
+        url: '/socials',
+        method: 'POST',
+        body: payload,
+      }),
+      invalidatesTags: ['Social'],
+    }),
+
+    getEmojis: builder.query<Emoji[], void>({
+      query: () => ({ url: '/socials/emojis' }),
+      providesTags: ['Emoji'],
+    }),
+  }),
+});
+
+export const {
+  useCreateSocialMutation,
+  useGetEmojisQuery,
+  useGetSocialsQuery,
+  useGetSocialByUuidQuery,
+} = socialApi;
