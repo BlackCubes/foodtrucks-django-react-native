024185bd4c38ec9172a68b2cee53c7857f276f5d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-01-17 13:40:13 -0800

    feat(auth-slice): create slice for auth and add it to store reducer

---
+++ web/src/features/auth/authSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import * as authLocalStorage from './authLocalStorage';
+
+import * as jwt from './utils/jwt';
+
+import { RootState } from '../../app/redux';
+
+import { User } from '../../common/models';
+
+export interface AuthState {
+  token: string | null;
+  user: User | null;
+}
+
+const storedAuthState = authLocalStorage.getAuthState();
+
+const initialState: AuthState =
+  storedAuthState?.token &&
+  !jwt.isMalformed(storedAuthState.token) &&
+  !jwt.isExpired(storedAuthState.token)
+    ? storedAuthState
+    : { token: null, user: null };
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    userLoggedIn: (state: AuthState, action: PayloadAction<AuthState>) => {
+      const { token, user } = action.payload;
+
+      state.token = token;
+      state.user = user;
+    },
+
+    userLoggedOut: (state: AuthState) => {
+      state.token = null;
+      state.user = null;
+    },
+  },
+});
+
+export const selectAuthState = (state: RootState): AuthState => state.auth;
