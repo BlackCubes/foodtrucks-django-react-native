80667af135d5c617ccf0b553ae91a6f52f7ea9bc
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-01-16 23:13:10 -0800

    feat(token-user-interface): create type interfaces for the user and token

--- web/src/common/models/index.ts
+++ web/src/common/models/index.ts
@@ -2,4 +2,6 @@ export * from './event';
 export * from './foodtruck';
 export * from './product';
 export * from './review';
+export * from './session';
 export * from './social';
+export * from './user';
