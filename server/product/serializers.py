7953ce1c670e333fdd4b005da1e20f0fbf2a75da
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-01-18 21:11:52 -0800

    refactor(product-serializer): remove the 'likes' and 'reviews' fields

--- server/product/serializers.py
+++ server/product/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 
 from .models import Product
-from review.serializers import ReviewSerializer
-from social.serializers import LikeSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -15,11 +13,9 @@ class ProductSerializer(serializers.ModelSerializer):
     reviews, and likes.
     """
     truck = serializers.CharField(source='truck.slug')
-    likes = LikeSerializer(many=True, read_only=True)
-    reviews = ReviewSerializer(many=True, read_only=True)
 
     class Meta:
         model = Product
         lookup_field = 'slug'
         fields = ('uuid', 'name', 'slug', 'info', 'image', 'price',
-                  'quantity', 'is_available', 'truck', 'likes', 'reviews',)
+                  'quantity', 'is_available', 'truck',)
