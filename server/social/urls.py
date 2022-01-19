86059114764f0658a157dee9c20ca4a5e1dc6764
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-01-18 17:01:48 -0800

    refactor(remove-like-update): remove the update portion to only retrieve likes

--- server/social/urls.py
+++ server/social/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.LikeListCreateAPIView.as_view()),
-    path('<uuid:uuid>', views.LikeDetailUpdateAPIView.as_view()),
+    path('<uuid:uuid>', views.LikeDetailAPIView.as_view()),
     path('emojis/', views.EmojiListAPIView.as_view()),
     path('emojis/<uuid:uuid>', views.EmojiDetailAPIView.as_view()),
 ]
