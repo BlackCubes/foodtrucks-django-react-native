0c3e8ef66628fc438241e471f1bd73a444d3f54e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-01-19 13:37:01 -0800

    feat(foodtruck-profile-image): add  to the foodtruck API if that property is asked for by the developer

--- server/foodtruck/serializers.py
+++ server/foodtruck/serializers.py
@@ -32,8 +32,42 @@ class TruckSerializer(DynamicFieldsModelSerializer):
     """
     images = TruckImageSerializer(many=True, read_only=True)
 
+    want_profile_image = False
+
     class Meta:
         model = Truck
         lookup_field = 'slug'
         fields = ('uuid', 'name', 'slug', 'info', 'phone_number',
                   'email', 'website', 'images',)
+
+    def __init__(self, *args, **kwargs):
+        # An error would occur if this is not popped.
+        extra_fields = kwargs.pop('extra_fields', None)
+
+        # This is a safety check if `images` is in fields.
+        fields = kwargs.get('fields', None)
+
+        if extra_fields is not None and fields is not None:
+            self.want_profile_image = 'profile_image' in extra_fields and 'images' in fields
+
+        super(TruckSerializer, self).__init__(*args, **kwargs)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        if self.want_profile_image:
+            profile_image = None
+
+            if not representation['images']:
+                profile_image = None
+            else:
+                for image_fields in representation['images']:
+                    if image_fields['is_profile_image']:
+                        profile_image = image_fields['image']
+                        break
+
+            representation.pop('images')
+
+            representation['profile_image'] = profile_image
+
+        return representation
