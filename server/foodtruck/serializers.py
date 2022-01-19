7e2b389b8b03bd2c5da117a37e6126321fb96077
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-01-19 15:58:23 -0800

    refactor(custom-serializer): reduce the TruckSerializer by adding some functionalities to a custom class

--- server/foodtruck/serializers.py
+++ server/foodtruck/serializers.py
@@ -5,50 +5,22 @@ from .models import Truck, TruckImage
 from main.utils import DynamicFieldsModelSerializer
 
 
-class TruckImageSerializer(serializers.ModelSerializer):
+class GetTruckProfileImageDynamicSerializer(DynamicFieldsModelSerializer):
     """
-    Serializer on the TruckImage model.
+    A DynamicSerializer that gets the Foodtruck's profile image.
 
-    Lookup Field: slug.
-
-    Fields: uuid, image, is_profile_image, and truck.
-    """
-    truck = serializers.CharField(source='truck.slug')
-
-    class Meta:
-        model = TruckImage
-        lookup_field = 'uuid'
-        fields = ('uuid', 'image', 'is_profile_image', 'truck',)
-
-
-class TruckSerializer(DynamicFieldsModelSerializer):
-    """
-    Serializer on the Truck model. If the `profile_image` is required, then when
-    initializing this serializer, add in `fields` with `images` and `extra_fields`
-    with `profile_image` as kwargs:
+    If the `profile_image` is required, then when initializing the `TruckSerializer`,
+    add in `fields` with `images` and `extra_fields` with `profile_image` as kwargs:
         `TruckSerializer(fields=('images',), extra_fields=('profile_image',))`.
 
     The `__init__` method is used to check if both `profile_image` and `images` are
     in the `extra_fields` and `fields` (respectively) to get the Truck's profile image
-    in `to_representation`.
+    in the `to_representation` method.
 
     The `to_representation` method is used to actually get the Truck's profile image, and
     set it in the JSON response while removing the `images` output.
-
-    Lookup Field: slug.
-
-    Fields: uuid, name, slug, info, phone_number, email, website, products, images,
-    and events.
     """
-    images = TruckImageSerializer(many=True, read_only=True)
-
     want_profile_image = False
-
-    class Meta:
-        model = Truck
-        lookup_field = 'slug'
-        fields = ('uuid', 'name', 'slug', 'info', 'phone_number',
-                  'email', 'website', 'images',)
 
     def __init__(self, *args, **kwargs):
         # An error would occur if this is not popped.
@@ -60,7 +32,8 @@ class TruckSerializer(DynamicFieldsModelSerializer):
         if extra_fields is not None and fields is not None:
             self.want_profile_image = 'profile_image' in extra_fields and 'images' in fields
 
-        super(TruckSerializer, self).__init__(*args, **kwargs)
+        super(GetTruckProfileImageDynamicSerializer,
+              self).__init__(*args, **kwargs)
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
@@ -81,3 +54,40 @@ class TruckSerializer(DynamicFieldsModelSerializer):
             representation['profile_image'] = profile_image
 
         return representation
+
+
+class TruckImageSerializer(serializers.ModelSerializer):
+    """
+    Serializer on the TruckImage model.
+
+    Lookup Field: slug.
+
+    Fields: uuid, image, is_profile_image, and truck.
+    """
+    truck = serializers.CharField(source='truck.slug')
+
+    class Meta:
+        model = TruckImage
+        lookup_field = 'uuid'
+        fields = ('uuid', 'image', 'is_profile_image', 'truck',)
+
+
+class TruckSerializer(GetTruckProfileImageDynamicSerializer):
+    """
+    Serializer on the Truck model. If the `profile_image` is required, then when
+    initializing this serializer, add in `fields` with `images` and `extra_fields`
+    with `profile_image` as kwargs:
+        `TruckSerializer(fields=('images',), extra_fields=('profile_image',))`.
+
+    Lookup Field: slug.
+
+    Fields: uuid, name, slug, info, phone_number, email, website, products, images,
+    and events.
+    """
+    images = TruckImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Truck
+        lookup_field = 'slug'
+        fields = ('uuid', 'name', 'slug', 'info', 'phone_number',
+                  'email', 'website', 'images',)
