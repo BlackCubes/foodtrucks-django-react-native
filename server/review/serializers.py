b1dc5481348057a35dcd576a13dd292244600daa
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-01-20 22:02:51 -0800

    refactor(custom-class): create custom classes to keep code cleaner with docs

--- server/review/serializers.py
+++ server/review/serializers.py
@@ -9,7 +9,89 @@ from product.models import Product
 from user.models import CustomUser
 
 
-class ReviewSerializer(serializers.ModelSerializer):
+class ProductRepresentationModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that transforms the `product` field to return an object
+    for better description. In the `ReviewSerializer`, the fields (not the `fields`
+    in `Meta`) of `product` has a relational serializer field which is the
+    `SlugRelatedField`, and this would have an affect on the field to return the
+    `slug_field` parameter. This leads to less description. The solution is to
+    simply implement `to_representation` method for a better output description on
+    this field.
+    """
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        # Checks to see if the `image` field is not empty, or else it won't serialize.
+        product_image = None if not instance.product.image else instance.product.image
+
+        representation['product'] = {
+            'uuid': str(instance.product.uuid),
+            'name': instance.product.name,
+            'slug': instance.product.slug,
+            'image': product_image,
+        }
+
+        return representation
+
+
+class RemoveUserRepresentationSerializer(ProductRepresentationModelSerializer):
+    """
+    A RepresentationSerializer that removes the `user` field from the outout in
+    order to avoid redundancy since we already know what type of authenticated
+    user the list of reviews belongs to. This is known if the user is logged in
+    and enters the route: `/api/v1/reviews/my-reviews`.
+
+    If one wants to know the detail of this authenticated user, they simply call another
+    route: `/api/v1/users/profile`.
+
+    In other words, this route is only concern about getting all reviews from the
+    currently logged in authenticated user.
+
+    The route this only targets: `/api/v1/reviews/my-reviews`.
+    """
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        is_auth_user_reviews_route = get_request_view_name(
+            self.context['request'].path) == 'reviews:my-reviews'
+
+        if is_auth_user_reviews_route:
+            representation.pop('user')
+
+        return representation
+
+
+class RemoveProductRepresentationSerializer(RemoveUserRepresentationSerializer):
+    """
+    A RepresentationSerializer that removes the `product` field from the output in
+    order to avoid redundancy since we already know what type of product the list
+    of reviews belongs to. This is known by the product's slug from the nested route:
+    `/api/v1/products/<slug>/reviews`.
+
+    If one wants to know the detail of this product, they simply call another route:
+    `/api/v1/products/<slug>`.
+
+    In other words, this route is only concern about getting all reviews from the product.
+
+    The route this only targets: `/api/v1/products/<slug>/reviews`.
+    """
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        is_reviews_in_product_route = get_request_view_name(
+            self.context['request'].path) == 'products:review-list'
+
+        if is_reviews_in_product_route:
+            representation.pop('product')
+
+        return representation
+
+
+class ReviewSerializer(RemoveProductRepresentationSerializer):
     """
     Serializer on the Review model.
 
@@ -36,35 +118,3 @@ class ReviewSerializer(serializers.ModelSerializer):
         lookup_field = 'uuid'
         fields = ('uuid', 'review', 'created_at',
                   'updated_at', 'product', 'user',)
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-
-        # This is to check if it is on the route `/api/v1/reviews/my-reviews/.`
-        is_auth_user_reviews_route = get_request_view_name(
-            self.context['request'].path) == 'reviews:my-reviews'
-
-        if is_auth_user_reviews_route:
-            # If it is in the route `/api/v1/reviews/my-reviews/`, then remove
-            # the `user` key since this route shows all reviews made by the
-            # current authenticated user (removes redundancy).
-            representation.pop('user')
-
-        # This is to check if it is on the route `/api/v1/products/<slug>/reviews/.`
-        is_reviews_in_product_route = get_request_view_name(
-            self.context['request'].path) == 'products:review-list'
-
-        if is_reviews_in_product_route:
-            representation.pop('product')
-        else:
-            # Checks to see if the `image` field is not empty, or else it won't serialize.
-            product_image = None if not instance.product.image else instance.product.image
-
-            representation['product'] = {
-                'uuid': str(instance.product.uuid),
-                'name': instance.product.name,
-                'slug': instance.product.slug,
-                'image': product_image,
-            }
-
-        return representation
