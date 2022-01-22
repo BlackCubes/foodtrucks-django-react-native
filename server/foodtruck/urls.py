b052e519fc7389c18be2dfdacdc8c6dbe59e1964
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-01-21 16:52:15 -0800

    feat(foodtruck-events-url): create nested URL on getting all events from foodtruck

--- server/foodtruck/urls.py
+++ server/foodtruck/urls.py
@@ -8,6 +8,8 @@ app_name = 'foodtrucks'
 urlpatterns = [
     path('', views.TruckListAPIView.as_view(), name='list'),
     path('<slug:slug>', views.TruckDetailAPIView.as_view(), name='detail'),
+    re_path(r'^(?P<truck_slug>[\w-]+)/events/?$',
+            views.TruckEventsModelViewSet.as_view({'get': 'list'}), name='event-list'),
     re_path(r'^(?P<truck_slug>[\w-]+)/products/?$',
             views.TruckProductsModelViewSet.as_view({'get': 'list'}), name='product-list'),
     re_path(r'^(?P<truck_slug>[\w-]+)/reviews/?$',
