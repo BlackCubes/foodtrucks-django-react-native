54a3240df49e94ad72feadf376548596ab984743
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-01-24 12:47:32 -0800

    refactor(custom-class-pagination): add docs

--- server/main/pagination.py
+++ server/main/pagination.py
@@ -6,6 +6,17 @@ from rest_framework.response import Response
 
 class CustomPageNumberPagination(pagination.PageNumberPagination):
     """
+    A custom PageNumberPagination to be used for all API apps with a list of data.
+    If there is a list of data, the `meta_data` key would be inserted to the JSON output
+    that is an object with keys of `total_count` (total count of the list of data for all
+    pages), `next` (a hyperlink for the next result, or `None`), and `previous` (a hyperlink
+    for the previous result, or `None`. `None` for the initial start).
+
+    Default count per page: 10.
+
+    Query param to change the count per page: `count`.
+
+    Maximum amount of pages: 100.
     """
     page_size = 10
     page_size_query_param = 'count'
