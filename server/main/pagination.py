1e1d6a9ac5c5d9a0ee2c37a1bac0091d5b459f03
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-01-24 12:11:38 -0800

    refactor(custom-pagination-class): remove

--- server/main/pagination.py
+++ server/main/pagination.py
@@ -10,7 +10,6 @@ class CustomPageNumberPagination(pagination.PageNumberPagination):
     page_size = 1
     page_size_query_param = 'count'
     max_page_size = 100
-    page_query_param = 'p'
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
