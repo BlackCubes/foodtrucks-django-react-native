08ae0f1b6a8d5104b1ea08b188a3fd2d94155a73
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-06 22:05:00 -0700

    feat(next-config): add configs for styled-components and PWA

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+module.exports = withPWA({
+  compiler: {
+    styledComponents: true,
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  reactStrictMode: true,
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    register: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+    skipWaiting: true,
+  },
+});
