5dc45bb132c9238203e72d4711208803845acb2c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-06 21:19:03 -0700

    refactor(web-folder): remove folder and file

--- web/src/common/models/event.ts
+++
@@ -1,9 +0,0 @@
-import { NestedFoodtruck } from './foodtruck';
-
-export interface Event {
-  uuid: string;
-  date: string;
-  start_time: string;
-  end_time: string;
-  truck: NestedFoodtruck[] | [];
-}
